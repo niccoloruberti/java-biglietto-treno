80f95c0b6f04eab0378e6de0dd0b0151f8d63ce3
niccoloruberti <nikiruberti@gmail.com>
2023-11-09 17:38:46 +0100

    richiesta dei dati all'utente

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,28 @@
 package org.lessons.java;
 
-public class CalcolaBiglietto {
+import java.util.Scanner;
 
+public class CalcolaBiglietto {
+	
+	public static void main(String[] args) {
+	
+		Scanner in = new Scanner(System.in);
+		
+		//chiedo all'utente quanti Km vuole percorrere
+		System.out.println("Quanti Km vuoi percorrere?");
+		String kmStr = in.nextLine();
+		//converto i km da stringa a numero
+		int kmInt = Integer.valueOf(kmStr);
+		
+		//chiedo all'utente quanti Km vuole percorrere
+		System.out.println("Quanti anni hai?");
+		String ageStr = in.nextLine();
+		//converto gli anni da stringa a numero
+		int ageInt = Integer.valueOf(ageStr);
+		
+		System.out.println(kmInt);
+		System.out.println(ageInt);
+	
+	}
+	
 }
